82f58e21e6f80943f544bc02dca2566abaeb931b
Bhim Raj Rai <02230056.cst@rub.edu.bt>
2024-02-29 11:41:36 +0600

    make changes in class activity.py

---
+++ class activity.py
@@ -0,0 +1,19 @@
+print("Class Activity")
+
+name= str(input("Enter your name:"))
+print(name)
+
+
+num1= float(input("Enter your number:"))
+num2= float(input("Enter your number:"))
+print(num1+num2)
+print(num1*num2)
+print(num1+num2)
+print(num1/num2)
+
+
+
+
+
+
+
